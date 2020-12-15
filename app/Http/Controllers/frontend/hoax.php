a34eb111a8b3ea447765ce26a4cea7683688f727
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2020-12-15 08:23:23 +0700

    Update hoax.php

--- app/Http/Controllers/frontend/hoax.php
+++ app/Http/Controllers/frontend/hoax.php
@@ -28,6 +28,21 @@ class hoax extends Controller
         
     }
 
+    public function index3()
+    {
+   
+        $beritahoax = DB::table('hoax')
+        ->join('gallery','gallery.id_gallery','hoax.gambar')
+        ->join('laporan_kategori','laporan_kategori.id_kategori','hoax.id_kategori')
+        ->select('hoax.id_hoax','hoax.judul','laporan_kategori.kategori'
+        ,'hoax.sumber','hoax.tanggal_upload','hoax.gambar','gallery.img','hoax.content')
+        ->orderBy('id_hoax', 'desc')
+        ->paginate(2);
+        return view('frontend.hoax.hoax', ['beritahoax' => $beritahoax]);
+
+        
+    }
+
 
 
     public function edit2($id)
