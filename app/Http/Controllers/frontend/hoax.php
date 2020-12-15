4dc7f93c95c507a926f90882bfb32950fd418912
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2020-12-15 09:02:55 +0700

    njier
    asdfadf

--- app/Http/Controllers/frontend/hoax.php
+++ app/Http/Controllers/frontend/hoax.php
@@ -13,6 +13,20 @@ use \App\Gallery_categories_model;
 
 class hoax extends Controller
 {
+    public function index()
+    {
+   
+        $beritahoax = DB::table('hoax')
+        ->join('gallery','gallery.id_gallery','hoax.gambar')
+        ->join('laporan_kategori','laporan_kategori.id_kategori','hoax.id_kategori')
+        ->select('hoax.id_hoax','hoax.judul','laporan_kategori.kategori'
+        ,'hoax.sumber','hoax.tanggal_upload','hoax.gambar','gallery.img','hoax.content')        
+        ->orderBy('id_hoax', 'desc')
+        ->paginate(2);
+        return view('frontend.hoax.hoax', ['beritahoax' => $beritahoax]);
+
+        
+    }
     public function index2()
     {
    
@@ -21,6 +35,7 @@ class hoax extends Controller
         ->join('laporan_kategori','laporan_kategori.id_kategori','hoax.id_kategori')
         ->select('hoax.id_hoax','hoax.judul','laporan_kategori.kategori'
         ,'hoax.sumber','hoax.tanggal_upload','hoax.gambar','gallery.img','hoax.content')
+        ->where('hoax.id_kategori','=',5)
         ->orderBy('id_hoax', 'desc')
         ->paginate(2);
         return view('frontend.hoax.hoax', ['beritahoax' => $beritahoax]);
@@ -35,8 +50,9 @@ class hoax extends Controller
         ->join('gallery','gallery.id_gallery','hoax.gambar')
         ->join('laporan_kategori','laporan_kategori.id_kategori','hoax.id_kategori')
         ->select('hoax.id_hoax','hoax.judul','laporan_kategori.kategori'
-        ,'hoax.sumber','hoax.tanggal_upload','hoax.gambar','gallery.img','hoax.content')
+        ,'hoax.sumber','hoax.tanggal_upload','hoax.gambar','gallery.img','hoax.content')        
         ->orderBy('id_hoax', 'desc')
+        ->where('hoax.id_kategori','=',4)
         ->paginate(2);
         return view('frontend.hoax.hoax', ['beritahoax' => $beritahoax]);
 
@@ -53,7 +69,7 @@ class hoax extends Controller
         $laphoax = DB::table('hoax')->where('id_hoax',$id)->join('gallery','gallery.id_gallery','hoax.gambar')
         ->join('laporan_kategori','laporan_kategori.id_kategori','hoax.id_kategori')
         ->select('hoax.id_hoax','hoax.judul','laporan_kategori.kategori'
-        ,'hoax.sumber','hoax.tanggal_upload','hoax.gambar','gallery.img','hoax.content')
+        ,'hoax.sumber','hoax.tanggal_upload','hoax.gambar','gallery.img','hoax.content')        
         ->orderBy('id_hoax', 'desc')
         ->get();
         return view('frontend.hoax.BeritaHoaxDetil',['gallery' => $gallery, 'categories' => $gallery_categories, 'laporan_kategori' => $lap_categories,'hoax' => $laphoax]);
--- app/Http/Controllers/frontend/hoax2.php
+++ app/Http/Controllers/frontend/hoax2.php
@@ -13,26 +13,50 @@ use \App\Gallery_categories_model;
 
 class hoax2 extends Controller
 {
-    public function index2()
+    public function index()
     {
-
+        //for home
         $beritahoax = DB::table('hoax')
         ->join('gallery','gallery.id_gallery','hoax.gambar')
         ->join('laporan_kategori','laporan_kategori.id_kategori','hoax.id_kategori')
         ->select('hoax.id_hoax','hoax.judul','laporan_kategori.kategori'
         ,'hoax.sumber','hoax.tanggal_upload','hoax.gambar','gallery.img','hoax.content')
         ->orderBy('id_hoax', 'desc')
-        ->paginate(1);
+        ->paginate(4);
+       
+    }
+    public function index2()
+    {
+        //for hoax
+        $beritahoax = DB::table('hoax')
+        ->join('gallery','gallery.id_gallery','hoax.gambar')
+        ->join('laporan_kategori','laporan_kategori.id_kategori','hoax.id_kategori')
+        ->select('hoax.id_hoax','hoax.judul','laporan_kategori.kategori'
+        ,'hoax.sumber','hoax.tanggal_upload','hoax.gambar','gallery.img','hoax.content')
+        ->where('hoax.id_kategori','=',5)
+        ->orderBy('id_hoax', 'desc')
+        ->paginate(4);
         return view('newfrontend/KategoriHoax', ['beritahoax' => $beritahoax]);
 
-        
+    }
+    public function index3()
+    {
+        //for disinformasi
+        $beritahoax = DB::table('hoax')
+        ->join('gallery','gallery.id_gallery','hoax.gambar')
+        ->join('laporan_kategori','laporan_kategori.id_kategori','hoax.id_kategori')
+        ->select('hoax.id_hoax','hoax.judul','laporan_kategori.kategori'
+        ,'hoax.sumber','hoax.tanggal_upload','hoax.gambar','gallery.img','hoax.content')
+        ->where('hoax.id_kategori','=',5)
+        ->orderBy('id_hoax', 'desc')
+        ->paginate(4);
+        return view('newfrontend/KategoriHoax', ['beritahoax' => $beritahoax]);
 
     }
 
-
-
     public function edit2($id)
     {
+        //for detail
         $gallery = Gallery_model::all();
         $gallery_categories = Gallery_categories_model::all();
         $lap_categories = Laporan_kategori_model::all(['id_kategori','kategori']);
